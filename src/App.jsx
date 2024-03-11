a2f18d9ac8ccb8f7ad9f53650a5f5a0e95217557
harsh <harshjha08@outlook.com>
2024-03-12 02:11:02 +0530

    Added transition to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import linkedin from './assets/linkedin.svg'
 import github from './assets/github.svg'
 import leetcode from './assets/leetcode.svg'
 
+
 function App() {
-  
-  window.onload = function(){
-    window.scrollTo(0 , 0);
+
+  window.onload = function () {
+    window.scrollTo(0, 0);
   };
 
   const [activeLink, setActiveLink] = useState(0);  // Hook used to update the highlighting of navbar links based on which section is being viewed
+
+  const [menuIcon, setMenuIcon] = useState(true);
+
+  const [closeIcon, setCloseIcon] = useState(false);
 
 
   return (
@@ -19,49 +24,80 @@ function App() {
 
       {/* Header / navbar */}
       <div className="flex flex-col h-screen" id="Intro">
-        <header className="bg-black fixed top-0 w-full">
-          <div className="px-4 py-5 flex justify-center md:justify-evenly shadow-lg shadow-slate-800">
-            <div>
-              <h1 className="font-bold text-xl hidden md:block">Portfolio</h1>
+        <header className="bg-black fixed w-full">
+          <div className={`px-6 md:px-4 py-3 md:py-5 flex justify-center md:items-center md:justify-evenly shadow-lg shadow-slate-800`}>
+            <div className={`${menuIcon ? 'absolute md:relative left-5 md:left-0 top-[20px] md:top-0 block' : 'hidden md:block'}`}>
+              <h1 className="font-bold text-xl">Portfolio</h1>
             </div>
-            <div>
-              <ul className="flex gap-5 md:gap-10">
+            <div className={`overflow-hidden ${menuIcon ? 'transition-max-h duration-500 max-h-0' : 'transition-max-h duration-1000 max-h-[1000px]'} md:max-h-[none] md:block text-xl md:text-base mt-8 mb-2 md:mt-0 md:mb-0`}>
+            {/* <div className={`${menuIcon ? 'md:block hidden' : 'block text-xl md:text-base mt-8 mb-2 md:mt-0 md:mb-0'}`}> */}
+              <ul className="flex md:flex-row flex-col md:mx-0 gap-5 md:gap-10 items-center">
                 <li>
-                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 0 ? 'text-white border-b-2' : ''}`} onClick={()=> setActiveLink(0)}>HOME</a>
+                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 0 ? 'text-white border-b-2' : ''}`} onClick={() => { setActiveLink(0); setMenuIcon(true); setCloseIcon(false) }}>HOME</a>
                 </li>
                 <li>
-                  <a href="#About" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 1 ? 'text-white border-b-2' : ''}`} onClick={()=> setActiveLink(1)}>ABOUT</a>
+                  <a href="#About" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 1 ? 'text-white border-b-2' : ''}`} onClick={() => { setActiveLink(1); setMenuIcon(true); setCloseIcon(false) }}>ABOUT</a>
                 </li>
                 <li>
-                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 2 ? 'text-white border-b-2' : ''}`} onClick={()=> setActiveLink(2)}>SKILLS</a>
+                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 2 ? 'text-white border-b-2' : ''}`} onClick={() => { setActiveLink(2); setMenuIcon(true); setCloseIcon(false) }}>SKILLS</a>
                 </li>
                 <li>
-                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 3 ? 'text-white border-b-2' : ''}`} onClick={()=> setActiveLink(3)}>PROJECTS</a>
+                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 3 ? 'text-white border-b-2' : ''}`} onClick={() => { { setActiveLink(3); setMenuIcon(true); setCloseIcon(false) } }}>PROJECTS</a>
                 </li>
                 <li>
-                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 4 ? 'text-white border-b-2' : ''}`} onClick={()=> setActiveLink(4)}>CONNECT</a>
+                  <a href="#" className={`text-gray-400 hover:text-white cursor-pointer font-mono font-semibold hover:border-b-2 border-b-blue-700 ${activeLink === 4 ? 'text-white border-b-2' : ''}`} onClick={() => { { setActiveLink(4); setMenuIcon(true); setCloseIcon(false) } }}>CONNECT</a>
                 </li>
-              </ul>
-            </div>
-            <div className="hidden md:block">
-              <ul className="flex gap-4">
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-white cursor-pointer">
+                <li className="md:hidden flex gap-4">
+                  <a href="#">
                     <img src={linkedin} alt="LinkedIn" width={30} title="LinkedIn" />
                   </a>
-                </li>
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a href="#">
                     <img src={github} alt="GitHub" width={30} title="GitHub" />
                   </a>
-                </li>
-                <li>
-                  <a href="#" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a href="#">
                     <img src={leetcode} alt="LeetCode" width={30} title="LeetCode" />
                   </a>
                 </li>
               </ul>
             </div>
+            <div className="hidden md:block">
+              <ul className="flex gap-4 ">
+                <li>
+                  <a href="#">
+                    <img src={linkedin} alt="LinkedIn" width={30} title="LinkedIn" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src={github} alt="GitHub" width={30} title="GitHub" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img src={leetcode} alt="LeetCode" width={30} title="LeetCode" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            {/* Menu Icon */}
+            {/* < div className={`block md:hidden ${menuIcon ? 'block' : 'hidden'}`}>
+              <button onClick={() => { setMenuIcon(false); setCloseIcon(true) }}><img src={menu} alt="Menu" width={32} /></button>
+            </div> */}
+            {/* Close Icon*/}
+            {/* <div className={`${closeIcon ? 'md:hidden block' : 'hidden'}`}>
+              <button onClick={() => { setMenuIcon(true); setCloseIcon(false) }}><img src={close} alt="Close" width={32} /></button>
+            </div>  */}
+
+            {/* Menu Icon / Close Icon */}
+            <div className="w-6 h-8 absolute right-5 top-[17px] block md:hidden" onClick={() => { setMenuIcon(menuIcon => !menuIcon); setCloseIcon(closeIcon => !closeIcon) }}>
+              <span className={`w-full h-1 bg-blue-700 block absolute top-1/2 left-1/2 transition-transform duration-500 ${menuIcon ? 'transform -translate-x-1/2 -translate-y-2.5' : 'transform -rotate-45 -translate-x-1/2 -translate-y-1/2'}`}></span>
+              <span className={`w-full h-1 bg-blue-700 block absolute top-1/2 left-1/2 transition duration-500 ${menuIcon ? 'transform -translate-x-1/2 -translate-y-1/2' : 'transform -rotate-180 -translate-x-1/2 -translate-y-1/2 opacity-0'}`}></span>
+              <span className={`w-full h-1 bg-blue-700 block absolute top-1/2 left-1/2 transition-transform duration-500 ${menuIcon ? 'transform -translate-x-1/2 translate-y-1.5' : 'transform rotate-45 -translate-x-1/2 -translate-y-1/2'}`}></span>
+            </div>
+
+
+
           </div>
         </header>
 
@@ -70,10 +106,10 @@ function App() {
           <section>
             <div className="grid grid-cols-1 md:grid-cols-2 p-16 border-0 md:border-x-2 border-slate-800">
               <div className="md:p-2">
-                <h2 className="font-bold text-2xl md:text-4xl">Hello, I am</h2>
+                <h2 className="text-2xl md:text-4xl font-serif">Hello, I am</h2>
                 <h2 className="font-bold text-4xl md:text-7xl mt-2 font-mono">Harsh Anand</h2>
                 <div className="w-1/4 h-1 bg-blue-500 mt-1 md:mt-2"></div>
-                <h2 className="font-bold text-2xl md:text-4xl mt-2 md:mt-4 text-blue-700">Full stack developer.</h2>
+                <h2 className="text-2xl md:text-4xl mt-2 md:mt-4 text-blue-700 font-serif">Full stack developer.</h2>
               </div>
               <div className="flex justify-center items-center mt-10 md:mt-0">
                 <img src={profile} alt="Profile" className="w-52 border-b-2 border-blue-700 pb-5" />
@@ -96,10 +132,10 @@ function App() {
           <div className="mx-5 md:mx-10 mb-5 md:mb-10 w-1/12 border border-blue-700"></div>
           <div className="grid grid-cols-1 md:grid-cols-2 ">
             <div className="p-5 md:p-10 text-lg md:text-2xl font-mono text-black">
-            Hey there! I'm a 4th-semester B.Tech in Computer Science and Engineering student at the <span className="text-blue-700">Indian Institute of Information Technology, Kalyani</span>. With a deep passion for technology and innovation , I'm proficient in languages like Java, Python, JavaScript, and C. I'm always eager to dive into new technologies within the realm of computer science. I'm also into gaming and would love to learn game development some day.
+              Hey there! I'm a 4th-semester B.Tech in Computer Science and Engineering student at the <span className="text-blue-700">Indian Institute of Information Technology, Kalyani</span>. With a deep passion for technology and innovation , I'm proficient in languages like Java, Python, JavaScript, and C. I'm always eager to dive into new technologies within the realm of computer science. I'm also into gaming and would love to learn game development some day.
             </div>
             <div className="p-5 md:p-10 text-lg md:text-2xl font-mono text-black">
-            In addition to my prowess in core languages, I'm well-versed in frontend development using HTML, CSS, and JavaScript, as well as backend development using Spring Boot. From front-end design to back-end architecture, I've honed my skills to cover every aspect of modern software development. I love taking on tough challenges! Every problem is a chance for me to learn and get better. With that being said , I consider myself as a <span className="text-blue-700">profound learner and problem solver</span>.
+              In addition to my prowess in core languages, I'm well-versed in frontend development using HTML, CSS, and JavaScript, as well as backend development using Spring Boot. From front-end design to back-end architecture, I've honed my skills to cover every aspect of modern software development. I love taking on tough challenges! Every problem is a chance for me to learn and get better. With that being said , I consider myself as a <span className="text-blue-700">profound learner and problem solver</span>.
             </div>
           </div>
         </section>
@@ -112,7 +148,7 @@ function App() {
 
       {/* Skills */}
       <div className="h-screen bg-white">
-        
+
       </div>
 
     </>
