94e34fac1cedc1c3bf750974e6f01fceea3cd7f1
Harsh <harshjha08@outlook.com>
2024-07-11 00:19:17 +0530

    Portfolio release (v1.0)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react"
+
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
